19b5424f7ebfdd2796491ea2e86b5e3a97461c28
Sopan Shewale <sopan.shewale@gmail.com>
2017-07-13 00:18:19 +0530

    first commit

---
+++ batch-one/tasks/hello_python_programer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print ('''
+Hello to Python Programmer
+This is: 
+
+- Python Course
+- Actually this is Programming Course
+
+''')
+
---
+++ batch-one/tasks/mean.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+number_one = int(input("Enter First Number: "))
+number_two = int(input("Enter Second Number: "))
+
+addition_numbers = number_one + number_two 
+number_mean = addition_numbers / 2
+
+print ("The mean of both numbers is: ", number_mean)
+
+
