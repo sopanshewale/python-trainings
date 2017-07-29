c863d3c022c3acc1133275f7d4bcd1a6adb19310
Sopan Shewale <sopan.shewale@gmail.com>
2017-07-29 14:00:38 +0530

    updating extend vs append script

---
+++ batch-one/day-2/lists_extend_vs_append.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+# append 
+x = [1, 2, 3]
+x.append([4, 5])
+print (x)
+
+# extend 
+
+x = [1, 2, 3]
+x.extend([4, 5])
+print (x)
+
+A = [1, 2, 3]
+B = [4, 5]
+
+print (A + B) 
+
+print (A * 3)
+
