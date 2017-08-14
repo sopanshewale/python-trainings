ad3bd3c2017045298ee4f7ddbfd56b702e7673f3
Sopan Shewale <sopan.shewale@gmail.com>
2017-08-14 11:41:15 +0530

    Binning code updated for NaN value

--- batch-one/follow-session/data_binning.py
+++ batch-one/follow-session/data_binning.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import numpy as np
 
+# Remember: 
+# Any NA values will be NA in the result. Out of bounds values will be NA in the resulting Categorical object
+
 
 np_array = np.array([25, 94, np.nan, 62, 70, np.nan, 94, 57, 62, 70, 62, 70])
 raw_data = {
@@ -15,7 +18,6 @@ raw_data = {
                     'Cooze', 'Jacon', 'Ryaner', 'Sone', 
                     'Sloan', 'Piger', 'Riani', 'Ali'],
         'preTestScore': [4, 24, 31, 2, 3, 4, 24, 31, 2, 3, 2, 3],
-        #'postTestScore': [25, 94, 58, 62, 70, 25, 94, 57, 62, 70, 62, 70]
         'postTestScore': np_array
         }
 df = pd.DataFrame(raw_data, columns = ['regiment', 'company', 'name', 'preTestScore', 'postTestScore'])
